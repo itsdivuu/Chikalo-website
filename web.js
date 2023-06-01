65407a641855c63ef559621bdf617240e579cbf4
Divya verma <133861343+itsdivuu@users.noreply.github.com>
2023-06-01 13:02:46 +0530

    Add files via upload

---
+++ web.js
@@ -0,0 +1,18 @@
+function myfunc(){
+    var navbar = document.getElementById("nav");
+    navbar.classList.toggle("show");
+}
+var modal = document.getElementById('id01');
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+var modal = document.getElementById('id02');
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
